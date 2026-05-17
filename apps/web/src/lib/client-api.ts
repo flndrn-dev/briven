cb3fb1e22e89f76ee52364dfb225e9c7b27e2b1f
flndrn-dev <flandriendev@hotmail.com>
2026-05-17 11:57:18 +0200

    fix: inline clientApiJson into auto-refresh component, remove client-api.ts

--- apps/web/src/lib/client-api.ts
+++
@@ -1,7 +0,0 @@
-import { apiOrigin } from './env';
-export async function clientApiJson(path, init = {}) {
-  const url = apiOrigin ? apiOrigin + path : path;
-  const res = await fetch(url, { ...init, headers: { 'content-type': 'application/json', ...init.headers }, credentials: 'include' });
-  if (!res.ok) throw new Error(await res.text().catch(() => ''));
-  return await res.json();
-}
